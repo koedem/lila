1e331a45c532ba855261873ff2bede06b29f17ca
Jonathan Gamble <101470903+schlawg@users.noreply.github.com>
2022-09-29 22:40:14 -0500

    color fixes moving on

--- ui/site/src/ask.ts
+++ ui/site/src/ask.ts
@@ -1,4 +1,5 @@
 import * as xhr from 'common/xhr';
+import throttle from 'common/throttle';
 
 // i have never been accused me of proper OOP
 
@@ -34,7 +35,7 @@ const rewire = (el: Element | null, frag: string): Ask | undefined => {
   }
 };
 
-const askXhr = (req: { ask: Ask; url: string; body?: FormData; process?: (_: Ask) => void }) =>
+const askXhr = (req: { ask: Ask; url: string; body?: FormData; after?: (_: Ask) => void }) =>
   xhr.textRaw(req.url, { method: 'post', /*redirect: 'error',*/ body: req.body }).then(
     async (rsp: Response) => {
       if (rsp.redirected) {
@@ -42,7 +43,7 @@ const askXhr = (req: { ask: Ask; url: string; body?: FormData; process?: (_: Ask
         else throw new Error(`Weirdness: ${rsp.url}`);
       }
       const newAsk = rewire(req.ask.el, await xhr.ensureOk(rsp).text());
-      if (req.process) req.process(newAsk!);
+      if (req.after) req.after(newAsk!);
     },
     (rsp: Response) => {
       console.log(`Ask XHR failed with ${rsp.status} ${rsp.statusText}`);
@@ -86,7 +87,7 @@ const wireSubmit = (ask: Ask): Element | undefined => {
       ask: ask,
       url: path,
       body: body,
-      process: ask => (ask.feedbackEl?.value ? 'success' : 'clean'),
+      after: ask => ask.feedbackState(ask.feedbackEl?.value ? 'success' : 'clean'),
     });
   });
   return submitEl;
@@ -101,13 +102,23 @@ const wireRankedChoices = (ask: Ask): void => {
   let initialOrder = ask.ranking();
   let d: DragContext;
 
-  const vertical: boolean = $('.ask__choices', ask.el) // wire the container
-    .on('dragover', (e: DragEvent) => updateCursor(d, e))
-    .on('dragleave', (e: DragEvent) => updateCursor(d, e))
-    .hasClass('vertical');
-
+  const container = $('.ask__choices', ask.el);
+  const vertical = container.hasClass('vertical');
   const [cursorEl, breakEl] = createCursor(vertical);
-  const updateCursor: (d: DragContext, e: DragEvent) => void = vertical ? updateCursorV : updateCursorH;
+  const updateCursor = throttle(100, (d: DragContext, e: DragEvent) => {
+    if (!d.isDone) vertical ? updateVCursor(d, e) : updateHCursor(d, e);
+    // avoid processing a delayed drag event after the drop
+  });
+
+  container
+    .on('dragover', (e: DragEvent) => {
+      e.preventDefault();
+      updateCursor(d, e);
+    })
+    .on('dragleave', (e: DragEvent) => {
+      e.preventDefault();
+      updateCursor(d, e);
+    });
 
   $('.ranked-choice', ask.el) // wire each draggable
     .on('dragstart', (e: DragEvent) => {
@@ -122,10 +133,12 @@ const wireRankedChoices = (ask: Ask): void => {
         cursorEl: cursorEl!,
         breakEl: breakEl,
         choices: Array.from($('.ranked-choice', ask.el), e => e!),
+        isDone: false,
       };
     })
     .on('dragend', (e: DragEvent) => {
       e.preventDefault();
+      d.isDone = true;
       d.dragEl.classList.remove('dragging');
       if (d.cursorEl.parentElement != d.parentEl) return;
       d.parentEl.insertBefore(d.dragEl, d.cursorEl);
@@ -137,7 +150,7 @@ const wireRankedChoices = (ask: Ask): void => {
       askXhr({
         ask: ask,
         url: path,
-        process: () => {
+        after: () => {
           initialOrder = newOrder;
         },
       });
@@ -145,12 +158,14 @@ const wireRankedChoices = (ask: Ask): void => {
 };
 
 type DragContext = {
-  dragEl: Element;
-  parentEl: Element;
-  box: DOMRect;
-  cursorEl: Element;
+  dragEl: Element; // we are dragging this
+  parentEl: Element; // ask__choices div
+  box: DOMRect; // the rectangle containing all choices
+  cursorEl: Element; // the I beam or <hr> depending on mode
   breakEl: Element | null; // null if vertical
   choices: Array<Element>;
+  isDone: boolean;
+  data?: any; // used to track dirty state in updateHCursor
 };
 
 const createCursor = (vertical: boolean) => {
@@ -168,11 +183,10 @@ const clearCursor = (d: DragContext): void => {
   if (d.breakEl?.parentNode) d.parentEl.removeChild(d.breakEl);
 };
 
-const updateCursorH = (d: DragContext, e: MouseEvent): void => {
-  e.preventDefault();
-
+const updateHCursor = (d: DragContext, e: MouseEvent): void => {
   if (e.x <= d.box.left || e.x >= d.box.right || e.y <= d.box.top || e.y >= d.box.bottom) {
     clearCursor(d);
+    d.data = null;
     return;
   }
   const rtl = document.dir == 'rtl';
@@ -180,26 +194,32 @@ const updateCursorH = (d: DragContext, e: MouseEvent): void => {
   for (let i = 0, lastY = 0; i < d.choices.length && !target; i++) {
     const r = d.choices[i].getBoundingClientRect();
     const x = r.right - r.width / 2;
-    const y = r.bottom + 4; // +4 because there's about 8 device px between rows
+    const y = r.bottom + 4; // +4 because there's (currently) 8 device px between rows
     const rowBreak = i > 0 && y != lastY;
-    if (rowBreak && e.y <= lastY && (rtl ? e.x >= d.box.left : e.x <= d.box.right))
+    if (rowBreak && e.y <= lastY)
+      // && (rtl ? e.x >= d.box.left : e.x <= d.box.right))
       target = { el: d.choices[i], break: 'afterend' };
     else if (e.y <= y && (rtl ? e.x >= x : e.x <= x))
       target = { el: d.choices[i], break: rowBreak ? 'beforebegin' : null };
     lastY = y;
   }
+  if (d.data && target && d.data.el == target.el && d.data.break == target.break) return; // nothing to do here
+
+  d.data = target; // keep last target in context data so we only diddle the DOM when dirty
+
   if (!target) {
     d.parentEl.insertBefore(d.cursorEl, null);
     return;
   }
   d.parentEl.insertBefore(d.cursorEl, target.el);
-
-  if (target.break) d.cursorEl.insertAdjacentElement(target.break, d.breakEl!);
-  else if (d.breakEl!.parentNode) d.parentEl.removeChild(d.breakEl!);
+  if (target.break) {
+    // fix a problem when inserting the cursor at the end of a line with no room
+    if (target.break != 'afterend' || d.cursorEl.getBoundingClientRect().top < e.y)
+      d.cursorEl.insertAdjacentElement(target.break, d.breakEl!);
+  } else if (d.breakEl!.parentNode) d.parentEl.removeChild(d.breakEl!);
 };
 
-const updateCursorV = (d: DragContext, e: DragEvent): void => {
-  e.preventDefault();
+const updateVCursor = (d: DragContext, e: DragEvent): void => {
   if (e.x <= d.box.left || e.x >= d.box.right || e.y <= d.box.top || e.y >= d.box.bottom) {
     clearCursor(d);
     return;
@@ -212,9 +232,23 @@ const updateCursorV = (d: DragContext, e: DragEvent): void => {
   d.parentEl.insertBefore(d.cursorEl, target);
 };
 
-/*const updateBadges = (ask: Ask): void => {
-  $('.ranked-choice', ask.el).each((index: number, el: HTMLElement) => {
-
-    el.innerText = `${index + 1}`;
+/* // tried this but don't really like it too much.
+const insertAnimation = (d: DragContext): void => {
+  const width = $(d.dragEl).innerWidth();
+  const el = d.dragEl as HTMLElement;
+  el.addEventListener('transitionend', () => {
+    el.style.transition = '';
+    el.style.flex = ''
+    el.style.overflow = '';
+    el.style.justifyContent = '';
   });
-};*/
+  d.parentEl.insertBefore(el, d.cursorEl);
+  clearCursor(d);
+  el.style.flex = '0 1 16px';
+  el.style.overflow = 'hidden';
+  el.style.justifyContent = 'center';
+  requestAnimationFrame(() => {
+    el.style.transition = 'flex 0.15s';
+    requestAnimationFrame(() => el.style.flex = `0 1 ${width}px`);
+  });
+}*/
